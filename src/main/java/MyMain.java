125296700704a9da71a1a7fb878042aae5db1313
milanboga <23mboga@athenian.org>
2021-09-15 22:28:07 -0700

    Finished first two methods, need help finishing third

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,28 +4,43 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if (ch >= 'A' && ch <= 'Z') {
+            ch =(char) (ch + 32);
+            return ch;
+        }
+        else {
+            return ch;
+        }
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if (ch >= 'a' && ch <= 'z') {
+            ch = (char) (ch - 32);
+            return ch;
+        }
+        else {
+            return ch;
+        }
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
+    //for loop that goes thorugh string one by one?
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        String empt = "";
+        int zero = str.indexOf('0');
+        if (zero != -1) {
+        }
+        return "";
     }
 
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+        System.out.println(toLower('j'));
+        System.out.println(toUpper('P'));
     }
 }
